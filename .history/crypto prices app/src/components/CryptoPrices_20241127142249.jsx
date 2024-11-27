fee25602776878bd3b9c309df58c03c6e0a7bab6
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2024-11-27 14:57:50 +0300

    commit

--- .history/crypto prices app/src/components/CryptoPrices_20241127142249.jsx
+++ .history/crypto prices app/src/components/CryptoPrices_20241127142249.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import './CryptoPrices.css';
 
 export function CryptoPrices() {
   const [cryptos, setCryptos] = useState([]);
--- .history/crypto prices app/src/components/CryptoPrices_20241127145352.jsx
+++ .history/crypto prices app/src/components/CryptoPrices_20241127145352.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import './CryptoPrices.css';
+import CryptoPrices './CryptoPrices.css'
 
 export function CryptoPrices() {
   const [cryptos, setCryptos] = useState([]);
